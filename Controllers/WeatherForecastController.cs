7d266a2b040a7330c3cbee2e8a5dc363f00db549
NonNontouch <nontouch16974@gmail.com>
2023-04-20 20:57:06 +0700

    Add Auth Support

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace my_new_app.Controllers;
 
+[Authorize]
 [ApiController]
 [Route("[controller]")]
 public class WeatherForecastController : ControllerBase
---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace my_new_app.Models;
+
+public class ApplicationUser : IdentityUser
+{
+}
