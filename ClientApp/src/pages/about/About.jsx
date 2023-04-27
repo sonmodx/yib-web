a890a1f91d3ccd1b2d607ecacd165f1b92beb1c9
Modx <sonmodx@gmail.com>
2023-04-27 23:26:34 +0700

    Update: Frontend
    Add about page
    Add css variable (--middle-fs)
    Edit navbar for mobile mode

--- ClientApp/src/pages/about/About.jsx
+++ ClientApp/src/pages/about/About.jsx
@@ -1,8 +1,58 @@
 import React from "react";
 import "./About.css";
+import pepperImg from "./image/pepper.png";
+import nonImg from "./image/non.png";
+const ProfileCard = ({ srcImg, name, role, nameID }) => {
+  return (
+    <div className="ProfileCard">
+      <img src={srcImg} alt="avatar" />
+      <p className="nameID">{nameID}</p>
+      <p className="name">{name}</p>
+      <p className="role">{role}</p>
+    </div>
+  );
+};
 
 const About = () => {
-  return <div className="About">About</div>;
+  return (
+    <div className="About">
+      <header>MEMBERS</header>
+      <div className="container">
+        <div className="grid">
+          <ProfileCard
+            srcImg={pepperImg}
+            nameID="64011281"
+            name="นายศัตชล หอมหวล"
+            role="Frontend"
+          />
+          <ProfileCard
+            srcImg={pepperImg}
+            nameID="64011281"
+            name="นายศัตชล หอมหวล"
+            role="Frontend"
+          />
+          <ProfileCard
+            srcImg={pepperImg}
+            nameID="64011281"
+            name="นายศัตชล หอมหวล"
+            role="Frontend"
+          />
+          <ProfileCard
+            srcImg={nonImg}
+            nameID="64011281"
+            name="นายศัตชล หอมหวล"
+            role="Frontend"
+          />
+          <ProfileCard
+            srcImg={nonImg}
+            nameID="64011281"
+            name="นายศัตชล หอมหวล"
+            role="Frontend"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
